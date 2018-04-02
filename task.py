fa4c10aa925cb669d4e134ab27f7458129e94cbd
Jaume Palencia <palencia.mail@gmail.com>
2018-04-02 13:10:43 +0200

    new task

--- task.py
+++ task.py
@@ -26,6 +26,9 @@ class Task():
         # Goal
         self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
 
+
+
+
     def get_reward(self):
         """Uses current pose of sim to return reward."""
         reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
